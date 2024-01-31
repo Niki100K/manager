dcabad25a04890dcdcdbcd7c7ac8a5e8ccb01ffd
Nikolai <niki_kaloianov@abv.bg>
2024-01-31 05:22:52 +0200

    add numbers

--- frontend/src/components/MainPage/hooks/Login.jsx
+++ frontend/src/components/MainPage/hooks/Login.jsx
@@ -3,10 +3,15 @@ import './Login.css'
 const Login = () => {
   const [niki100PassLogin, setNiki100PassLogin] = useState('')
   const handleForm = (e) => {
-    let value = e.target.value
-    value = value.replace(/\D/g, '')
-    value = value.length > 5 ? value.slice(0, 5) : value
-    setNiki100PassLogin(value)
+    if (typeof e === 'object') {
+      let value = e.target.value
+      value = value.replace(/\D/g, '')
+      value = value.length > 5 ? value.slice(0, 5) : value
+      setNiki100PassLogin(value)
+    } else {
+      
+      setNiki100PassLogin(niki100PassLogin.length < 5 ? niki100PassLogin + e : niki100PassLogin)
+    }
   }
   const [incorrectPass, setIncorrectPass] = useState(false)
   const handleLogin = (e) => {
@@ -17,6 +22,9 @@ const Login = () => {
       setIncorrectPass(true)
     }
   }
+  const numbers = [
+    '1234567890'
+  ]
   return (
     <div className='Login c r flex-c'>
       <div className='head c'>
@@ -41,6 +49,13 @@ const Login = () => {
             <p>Password is less then 5 symbols.</p>
           </div>
         }
+        <div className='numbers c'>
+          {numbers[0].split('').map((info, index) => (
+            <div onClick={() => handleForm(info)} className='number c r' key={index}>
+              <h1>{info}</h1>
+            </div>
+          ))}
+        </div>
         <div className='btn c'>
           <button type='button'>Settings</button>
           <button type='submit'>Login</button>
