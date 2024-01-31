a2ca9b9260d96e88d2b3031d75b6fd2eef0affa6
Nikolai <niki_kaloianov@abv.bg>
2024-01-31 04:52:31 +0200

    add login Page

---
+++ frontend/src/components/MainPage/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './MainPage.css'
+import Login from './hooks/Login'
+const MainPage = () => {
+  return (
+    <div className='MainPage c'>
+      <Login />
+    </div>
+  )
+}
+
+export default MainPage
---
+++ frontend/src/components/MainPage/hooks/Login.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import './Login.css'
+const Login = () => {
+  const [niki100PassLogin, setNiki100PassLogin] = useState('')
+  const handleForm = (e) => {
+    let value = e.target.value
+    value = value.replace(/\D/g, '')
+    value = value.length > 5 ? value.slice(0, 5) : value
+    setNiki100PassLogin(value)
+  }
+  const [incorrectPass, setIncorrectPass] = useState(false)
+  const handleLogin = (e) => {
+    e.preventDefault()
+    if (niki100PassLogin.length === 5) {
+      setIncorrectPass(false)
+    } else {
+      setIncorrectPass(true)
+    }
+  }
+  return (
+    <div className='Login c r flex-c'>
+      <header>Niki100 Manager</header>
+      <form className='c flex-c' onSubmit={handleLogin} action="">
+        <div className='input c'>
+          <input 
+            className={`${niki100PassLogin.length === 5 && 'correct'}`}
+            type="text"
+            value={niki100PassLogin}
+            onChange={(e) => handleForm(e)}
+            placeholder='' 
+            required
+          />
+          <label htmlFor="">Password</label>
+        </div>
+        {incorrectPass &&
+          <div className='error c r'>
+            <p>Password is less then 5 symbols.</p>
+          </div>
+        }
+        <div className='btn c'>
+          <button type='button'>Settings</button>
+          <button type='submit'>Login</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Login
