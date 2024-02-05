a00e9163c8ec56515dab777467163466bae63b96
Nikolai <niki_kaloianov@abv.bg>
2024-02-05 12:39:01 +0200

    local storage update

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -1,11 +1,22 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState, useEffect } from 'react';
 
-export const Store = createContext()
+export const Store = createContext();
+
 export const StoreProvider = ({ children }) => {
-    const [storeData, setStoreData] = useState({});
-    return (
-        <Store.Provider value={{ storeData, setStoreData }}>
-            { children }
-        </Store.Provider>
-    )
-}
+  const [storeData, setStoreData] = useState(() => {
+    // Зареждане на storeData от localStorage при първоначално рендериране
+    const storedData = localStorage.getItem('storeData');
+    return storedData ? JSON.parse(storedData) : {};
+  });
+
+  useEffect(() => {
+    // Запазване на storeData в localStorage при промяна
+    localStorage.setItem('storeData', JSON.stringify(storeData));
+  }, [storeData]);
+
+  return (
+    <Store.Provider value={{ storeData, setStoreData }}>
+      {children}
+    </Store.Provider>
+  );
+};
